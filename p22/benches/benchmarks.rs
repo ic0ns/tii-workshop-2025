f9df4968a1b8f7246aef27aa88ae6290391b8f04
Robert Merget <robert.merget@tii.ae>
2025-04-15 12:37:12 +0400

    fixed clippy

--- p22/benches/benchmarks.rs
+++ p22/benches/benchmarks.rs
@@ -1,5 +1,5 @@
 #![feature(test)]
-use p22::calc::{celsius2fahrenheit, fahrenheit2celsius, fibonacci_loop, fibonacci_rec};
+use p22::calc::fibonacci_loop;
 
 extern crate test;
 use test::{Bencher, black_box};
